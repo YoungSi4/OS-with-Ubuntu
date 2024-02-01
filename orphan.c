b8672cd56ab2817b442f3275e7c83654386c5091
YoungSi4 <dksdudgns01@gmail.com>
2024-02-01 14:15:53 +0900

    day02 - fix & shm

--- orphan.c
+++ orphan.c
@@ -1,20 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<sys/wait.h>
 
 int main() {
 	pid_t childPid;
+	int i;
 	childPid = fork();
 
 	if(childPid > 0) {
 		printf("parent pid : %d\n", getpid());
-		sleep(30);
+		sleep(2);
 		printf("parent terminated\n");
 		exit(0);
 	}
 	else if(childPid == 0){
 		printf("child\n");
 		sleep(1);
+		for(i=0; i< 10; i++){
+			printf("child pid: %d, parent pid: %d\n", getpid(), getppid());
+			sleep(1);
+		}
 		printf("child terminated\n");
 		exit(0);
 	}
